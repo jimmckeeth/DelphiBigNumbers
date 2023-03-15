b171c76b6f94748f930f94304d20802d22fb0a28
Jim McKeeth <jim@mckeeth.org>
2023-03-15 02:55:45 -0600

    Update CompilerAndRTLVersions.pas
    Added Sydney and Alexandria
    Removed old unused version const values

--- Source/CompilerAndRTLVersions.pas
+++ Source/CompilerAndRTLVersions.pas
@@ -1,7 +1,7 @@
 unit CompilerAndRTLVersions;
 
-// See https://blog.dummzeuch.de/2018/12/02/conditional-compilation-for-various-delphi-versions/
-// Slightly modified
+// Inspired by https://blog.dummzeuch.de/2018/12/02/conditional-compilation-for-various-delphi-versions/
+// With modifications
 
 interface
 
@@ -9,49 +9,35 @@ const
 
   // Compiler versions
 
-  CompilerVersionDelphi6        = 14.0;
-  CompilerVersionDelphi7        = 15.0;
-  CompilerVersionDelphi71       = 15.0;
-  CompilerVersionDelphi8        = 16.0;
+  CompilerVersionDelphi2009       = 20.0;
+  CompilerVersionDelphi2010       = 21.0;
 
-  CompilerVersionDelphi2005     = 17.0;
-  CompilerVersionDelphi2006     = 18.0;
-  CompilerVersionDelphi2007     = 18.5;
-  CompilerVersionDelphi2007NET  = 19.0;
-  CompilerVersionDelphi2009     = 20.0;
-  CompilerVersionDelphi2010     = 21.0;
+  CompilerVersionDelphiXE         = 22.0;
+  CompilerVersionDelphiXE2        = 23.0;
+  CompilerVersionDelphiXE3        = 24.0;
+  CompilerVersionDelphiXE4        = 25.0;
+  CompilerVersionDelphiXE5        = 26.0;
+  ComplierVersionAppMethod1       = 26.5;
+  CompilerVersionDelphiXE6        = 27.0;
+  CompilerVersionDelphiXE7        = 28.0;
+  CompilerVersionDelphiXE8        = 29.0;
 
-  CompilerVersionDelphiXE       = 22.0;
-  CompilerVersionDelphiXE2      = 23.0;
-  CompilerVersionDelphiXE3      = 24.0;
-  CompilerVersionDelphiXE4      = 25.0;
-  CompilerVersionDelphiXE5      = 26.0;
-  ComplierVersionAppMethod1     = 26.5;
-  CompilerVersionDelphiXE6      = 27.0;
-  CompilerVersionDelphiXE7      = 28.0;
-  CompilerVersionDelphiXE8      = 29.0;
-
-  CompilerVersionDelphi10       = 30.0;
-  CompilerVersionDelphi100      = 30.0;
-  CompilerVersionDelphiSeattle  = 30.0;
-  CompilerVersionDelphi101      = 31.0;
-  CompilerVersionDelphiBerlin   = 31.0;
-  CompilerVersionDelphi102      = 32.0;
-  CompilerVersionDelphiTokyo    = 32.0;
-  CompilerVersionDelphi103      = 33.0;
-  CompilerVersionDelphiRio      = 33.0;
+  CompilerVersionDelphi10         = 30.0;
+  CompilerVersionDelphi100        = 30.0;
+  CompilerVersionDelphiSeattle    = 30.0;
+  CompilerVersionDelphi101        = 31.0;
+  CompilerVersionDelphiBerlin     = 31.0;
+  CompilerVersionDelphi102        = 32.0;
+  CompilerVersionDelphiTokyo      = 32.0;
+  CompilerVersionDelphi103        = 33.0;
+  CompilerVersionDelphiRio        = 33.0;
+  CompilerVersionDelphi104        = 34.0;
+  CompilerVersionDelphiSydney     = 34.0;
+  CompilerVersionDelphi110        = 35.0;
+  CompilerVersionDelphiAlexandria = 35.0;
 
   // RTL versions
 
-  RTLVersionDelphi6             = 14.0;
-  RTLVersionDelphi7             = 15.0;
-  RTLVersionDelphi71            = 15.0;
-  RTLVersionDelphi8             = 16.0;
-
-  RTLVersionDelphi2005          = 17.0;
-  RTLVersionDelphi2006          = 18.0;
-  RTLVersionDelphi2007          = 18.5;
-  RTLVersionDelphi2007NET       = 19.0;
   RTLVersionDelphi2009          = 20.0;
   RTLVersionDelphi2010          = 21.0;
 
@@ -74,6 +60,10 @@ const
   RTLVersionDelphiTokyo         = 32.0;
   RTLVersionDelphi103           = 33.0;
   RTLVersionDelphiRio           = 33.0;
+  RTLVersionDelphi104           = 34.0;
+  RTLVersionDelphiSydney        = 34.0;
+  RTLVersionDelphi110           = 35.0;
+  RTLVersionDelphiAlexandria    = 35.0;
 
 implementation
 
